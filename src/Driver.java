41ba5d25bf442a288693894a0d31d254fb934113
sean <lius25505@gmail.com>
2026-03-26 12:11:50 -0400

    adding firmata

--- src/Driver.java
+++ src/Driver.java
@@ -4,15 +4,37 @@ About:The driver program for the arduino hardware
 Created by:
 Date Modified:2/3/2026
  */
+
+import org.firmata4j.I2CDevice;
+import org.firmata4j.Pin;
+import org.firmata4j.firmata.FirmataDevice;
+import org.firmata4j.ssd1306.SSD1306;
+
+import java.io.IOException;
+import java.util.Timer;
 public class Driver {
     /*
     Method Name:Main
     About:The main driver
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        final int co2sensorPin = 16;//A2
+        final int moistureSensorPin = 14;//A0
+        final int waterPumPin = 7; //D7
+        final int buttonPin = 6;//D6
 
+        FirmataDevice arduino = new FirmataDevice("COM3");
+        arduino.start();
+        System.out.println("Board Started");
+        try {
+            arduino.ensureInitializationIsDone();
+        } catch (Exception e) {
+            System.out.println("unable to connect to board");
+        }
     }
-
+/*
+Goes off if the detected water level is lower than required
+ */
     public static void alarm(int interval){
 
     }
--- src/WaterLevelSensor.java
+++ src/WaterLevelSensor.java
@@ -1,4 +1,60 @@
-package PACKAGE_NAME;
-
+/*
+Modified Code from RobotDinosaurs to accomadate java implementation
+ */
 public class WaterLevelSensor {
+
+    /*
+  WaterLevelSensor
+  Gets a reading from a sensor and outputs the value and percentage of the level of water
+  on a serial monitor.
+*/
+    // Value for storing water level
+    int value = 0;
+    char printBuffer[128];
+    Pin waterLevelSensor =
+    public WaterLevelSensor(){
+
+    }
+
+    public void setup() {
+        pinMode(sensorPower, OUTPUT);
+
+        // Set to LOW so no power flows through the sensor
+        digitalWrite(sensorPower, LOW);
+
+        Serial.begin(9600);
+    }
+
+    public void loop()
+    {
+        digitalWrite(sensorPower, HIGH);  // Turn the sensor ON
+        delay(1000);              // wait 1000 milliseconds
+
+        value = analogRead(sensorPin); // get adc value
+        sprintf(printBuffer, "Value: %d", value);
+
+        //values based on calibration
+        if (value<=120){
+            sprintf(printBuffer+strlen(printBuffer), "= 0%%");
+        }
+        else if (value>120 && value<=135){
+            sprintf(printBuffer+strlen(printBuffer), "= 0%% - 25%%");
+        }
+        else if (value>135 && value<=145){
+            System.out.printf(printBuffer+strlen(printBuffer), "= 25%% - 50%%");
+        }
+        else if (value>145 && value<=150){
+            sprintf(printBuffer+strlen(printBuffer), "= 50%% - 75%%");
+        }
+        else if (value>150){
+            sprintf(printBuffer+strlen(printBuffer), "= 75%% - 100%%");
+        }
+
+        Serial.println(printBuffer);
+
+        // Do not power the sensor constantly, but power it only when you take the readings.
+        digitalWrite(sensorPower, LOW);   // Turn the sensor OFF
+
+        delay(5000);
+    }
 }
