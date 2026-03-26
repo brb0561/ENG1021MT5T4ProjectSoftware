001c7009b56da18fda3f05bf493fde5c3081a6f4
sean <lius25505@gmail.com>
2026-03-26 15:44:00 -0400

    ra

--- src/Driver.java
+++ src/Driver.java
@@ -9,6 +9,7 @@ import org.firmata4j.Pin;
 import org.firmata4j.firmata.FirmataDevice;
 
 import java.io.IOException;
+import java.util.Timer;
 
 public class Driver {
     /*
@@ -18,10 +19,11 @@ public class Driver {
 
     static final String portNumber = "COM3";
     public static void main(String[] args) throws IOException {
+        final int cycleDuration  = 4000;//
         final int waterLevelSensorPin = 14;//A0
         final int waterPumPin = 7; //D7
         final int buttonPin = 6;//D6
-
+        final int motorPin = 0;//temporary pin needs to be adjusted for grove board
         FirmataDevice arduino = new FirmataDevice(portNumber);
         arduino.start();
         System.out.println("Board Started");
@@ -30,11 +32,15 @@ public class Driver {
         } catch (Exception e) {
             System.out.println("unable to connect to board");
         }
+        //initialization of pins/devices.
         Pin button = arduino.getPin(buttonPin);
         Pin waterPump  = arduino.getPin(waterPumPin);
-
+        Pin motor = arduino.getPin(motorPin);
         Pin waterLevelSensor = arduino.getPin(waterLevelSensorPin);
+        waterLevelSensor.setMode(Pin.Mode.ANALOG);
         arduino.addEventListener(new buttonDetector(button,waterPump));
+        var cycle = new cycle(waterPump,motor, waterLevelSensor);
+        new Timer().schedule(cycle,0,cycleDuration);
     }
 /*
 Goes off if the detected water level is lower than required
--- src/cycle.java
+++ src/cycle.java
@@ -7,6 +7,7 @@ public class cycle extends TimerTask {
     Pin waterPump;
     Pin motor;
     Pin waterSensor;
+    final double waterThreshold = 30;//needs to be adjusted
     public cycle(Pin waterPump, Pin motor, Pin waterSensor){
         this.motor = motor;
         this.waterPump = waterPump;
@@ -14,10 +15,18 @@ public class cycle extends TimerTask {
     }
     @Override
     public void run(){
-
+        try {
+            while (readWaterLevel(this.waterSensor)<waterThreshold){//keeps pumping water till it reaches acceptable threshold.
+                this.waterSensor.setValue(1);
+                Thread.sleep(2000);
+                this.waterSensor.setValue(0);
+            }
+            //add motor spinning code here.
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
     public static double readWaterLevel(Pin waterLevelSensor) throws IOException {
-        waterLevelSensor.setMode(Pin.Mode.ANALOG);
         return convertWaterLevelValue(waterLevelSensor.getValue());
     }
     /*
