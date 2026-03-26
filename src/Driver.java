1051bfa40ec443c0fa8488c51f1a2af2cd79cb52
heerj <heerjashansingh@gmail.com>
2026-03-26 11:15:30 -0400

    first

--- src/Driver.java
+++ src/Driver.java
@@ -10,7 +10,7 @@ public class Driver {
     About:The main driver
      */
     public static void main(String[] args) {
-        System.out.println("wqdsa");
+
     }
     public static void alarm(int interval){
 
